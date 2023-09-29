00c8d0eed7c2da9a3066efbb9bc26471cd560fbb
prasmalla <malla.prasanna@gmail.com>
2023-09-29 16:59:34 -0400

    add vercel ssr / view transition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyles: false })],
+	integrations: [
+		react(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+	output: 'hybrid',
+	adapter: vercel({ webAnalytics: true }),
 });
